cd4e4b03a3887619218130a8d744f4cd4150bf83
jaspreetsingh <jaspreetsingh@192.168.10.180>
2016-04-06 15:16:32 +0530

    Add web.xml files in WEB-INF directory

--- src/main/java/com/shuttle/conf/WebConfig.java
+++ src/main/java/com/shuttle/conf/WebConfig.java
@@ -3,7 +3,6 @@ package com.shuttle.conf;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc //<mvc:annotation-driven />
