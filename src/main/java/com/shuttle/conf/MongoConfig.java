5e7765e764fd34a68accfbc661cdc806233a6ad7
sumitluthra <sumitluthra@192.168.10.166>
2016-04-14 13:54:52 +0530

    Properties File Configuration for Mongo DB credentials

--- src/main/java/com/shuttle/conf/MongoConfig.java
+++ src/main/java/com/shuttle/conf/MongoConfig.java
@@ -1,37 +1,44 @@
 package com.shuttle.conf;
 
-import com.mongodb.DBCollection;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 import java.util.Arrays;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 @Configuration
+@PropertySource("classpath:MongoConfig.properties")
 public class MongoConfig {
-//@Autowired
-//MongoOperations mongoOperation;
+
+    @Value("${mongodb.user}")
+    private String mongoUser;
+    @Value("${mongodb.password}")
+    private String mongoPassword;
+    @Value("${mongodb.db}")
+    private String mongoDb;
+    @Value("${mongodb.serverip}")
+    private String mongoServerIP;
 
     public @Bean
     MongoDbFactory mongoDbFactory() throws Exception {
 
-        MongoCredential credential = MongoCredential.createScramSha1Credential("root", "shuttle", "mindfire".toCharArray());
-        ServerAddress serverAddress = new ServerAddress("127.0.0.1");
+        MongoCredential credential = MongoCredential.
+                createScramSha1Credential(mongoUser, mongoDb, mongoPassword.toCharArray());
+
+        ServerAddress serverAddress = new ServerAddress(mongoServerIP);
 
         MongoClient mongo = new MongoClient(serverAddress, Arrays.asList(credential));
 
-        SimpleMongoDbFactory simpleMongoDbFactory
-                = new SimpleMongoDbFactory(mongo, "shuttle"//,
-                );
+        SimpleMongoDbFactory simpleMongoDbFactory =
+                new SimpleMongoDbFactory(mongo, mongoDb);
 
         return simpleMongoDbFactory;
 
@@ -49,8 +56,8 @@ public class MongoConfig {
 
     }
 
-//        @Bean
-//    public DBCollection users() {
-//        return mongoOperation.getCollection("users");
-//    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
