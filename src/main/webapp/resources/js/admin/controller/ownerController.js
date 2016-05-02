dd5da27decb96ec2b296fc61f4936da1221a9ebf
baldeep <baldeep@baldeeps>
2016-05-02 11:56:36 +0530

    UI

--- src/main/webapp/resources/js/admin/controller/ownerController.js
+++ src/main/webapp/resources/js/admin/controller/ownerController.js
@@ -2,10 +2,10 @@ adminApp.controller('ownerController', function ($scope, $http) {
     $scope.owner = {};
     $scope.myWelcome = {};
     $scope.ownerHidden = false;
-    $scope.title="Amit";
+    
     
     $scope.hideDialog = function () {
-        $scope.title="Amit2";//modalTitle;
+
         $scope.ownerHidden = !$scope.ownerHidden;
     };
     $scope.getOwnerList = function () {
