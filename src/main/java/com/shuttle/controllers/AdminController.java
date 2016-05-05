65cfff52b7c146800b2f703f4d7aad665dc66acc
reddot_amitpandey <amit.pandey@>
2016-05-05 19:24:17 +0530

    Add custom method for owners  pull by role and changed Admin controller.

--- src/main/java/com/shuttle/controllers/AdminController.java
+++ src/main/java/com/shuttle/controllers/AdminController.java
@@ -5,7 +5,9 @@
 package com.shuttle.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.BasicDBObject;
 import com.shuttle.bean.OwnerBean;
+import com.shuttle.bean.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,34 +21,44 @@ import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.shuttle.constants.ShuttleConstants;
+import com.shuttle.repository.RoleRepository;
+import com.shuttle.repository.UserRepository;
+import com.shuttle.service.UserService;
+import com.shuttle.util.GeneratePassword;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private UserService userService;
+    
+  
+    
     @Autowired
     private OwnerRepository ownerRepository;
 
     @RequestMapping(value = ShuttleConstants.ADMIN_ADD_OWNER, method = RequestMethod.POST)
     @ResponseBody
     public String insertOwner(@RequestBody final String owner) {
+        Boolean result = false;
         try {
-
-            OwnerBean s = new ObjectMapper().readValue(owner, OwnerBean.class);
-            if (owner != null) {
-                
-                ownerRepository.save(s);
-                return "success";
-            }
-        } catch (IOException ex) {
+            UserBean s = new ObjectMapper().readValue(owner, UserBean.class);
+            s.setUserRole("ROLE_OWNER");
+            //s.setUserRole(roleRepository.findByRoleName("ROLE_OWNER"));
+            result=userService.insertUser(s);
+        } catch (Exception ex) {
             Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return "failure";
+        return (result?"success":"failure");
     }
 
     @RequestMapping(value = ShuttleConstants.ADMIN_GET_OWNERS, method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<OwnerBean> getOwnerList() {
-
-        return ownerRepository.findAll();
+    public List<UserBean> getOwnerList() {
+        List<UserBean> users=userRepository.findByUserRole("ROLE_OWNER");
+        return users;
     }
 }
--- src/main/java/com/shuttle/repository/UserRepository.java
+++ src/main/java/com/shuttle/repository/UserRepository.java
@@ -5,12 +5,14 @@
 package com.shuttle.repository;
 
 import com.shuttle.bean.UserBean;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<UserBean,String>{
-
+public interface UserRepository extends MongoRepository<UserBean,String> {
+     List<UserBean> findByUserRole(String role);
      UserBean findByUserEmail(String userEmail);
      UserBean findTopByOrderByUserIdDesc();
 }
